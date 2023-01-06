5372ec39bd6b810533977a34f47949d75c192f93
Vu Tri Sy <sy.vt194830@sis.hust.edu.vn>
2023-01-06 14:20:51 +0700

    6/1

---
+++ assignment-1-review-of-linked-data-LinhHuynh17-main/assignment-1-review-of-linked-data-LinhHuynh17-main/WebpageNetwork.h
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class WebpageNetwork{
+    public:
+
+        void AddFiles(vector<string> files){
+
+        }
+
+        void InteractiveMode(string filename){
+
+        }
+
+        void RemoveFiles(vector<string> files);
+};
